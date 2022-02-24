7a2da8fd1311f85d78a5d947a947d5d248c93f88
superrob3000 <robbracero@gmail.com>
2022-02-24 16:28:44 -0500

    Add project files.

---
+++ OmegaBigBoxMonitor/MonitorForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace OmegaBigBoxMonitor
+{
+    partial class MonitorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MonitorForm));
+            this.TextBox_Events = new System.Windows.Forms.TextBox();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ShowMonitorMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hide_button = new System.Windows.Forms.Button();
+            this.kill_button = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox_Events
+            // 
+            this.TextBox_Events.BackColor = System.Drawing.Color.White;
+            this.TextBox_Events.Location = new System.Drawing.Point(12, 12);
+            this.TextBox_Events.Multiline = true;
+            this.TextBox_Events.Name = "TextBox_Events";
+            this.TextBox_Events.ReadOnly = true;
+            this.TextBox_Events.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox_Events.Size = new System.Drawing.Size(776, 426);
+            this.TextBox_Events.TabIndex = 0;
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Omega BigBox Monitor";
+            this.trayIcon.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ShowMonitorMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(150, 26);
+            // 
+            // ShowMonitorMenuItem
+            // 
+            this.ShowMonitorMenuItem.Name = "ShowMonitorMenuItem";
+            this.ShowMonitorMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.ShowMonitorMenuItem.Text = "Show Monitor";
+            this.ShowMonitorMenuItem.Click += new System.EventHandler(this.ShowMonitorMenuItem_Click);
+            // 
+            // hide_button
+            // 
+            this.hide_button.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.hide_button.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hide_button.Location = new System.Drawing.Point(223, 466);
+            this.hide_button.Name = "hide_button";
+            this.hide_button.Size = new System.Drawing.Size(161, 42);
+            this.hide_button.TabIndex = 2;
+            this.hide_button.Text = "Hide Monitor";
+            this.hide_button.UseVisualStyleBackColor = false;
+            this.hide_button.Click += new System.EventHandler(this.hide_button_Click);
+            // 
+            // kill_button
+            // 
+            this.kill_button.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.kill_button.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kill_button.Location = new System.Drawing.Point(414, 466);
+            this.kill_button.Name = "kill_button";
+            this.kill_button.Size = new System.Drawing.Size(161, 42);
+            this.kill_button.TabIndex = 3;
+            this.kill_button.Text = "Kill Monitor";
+            this.kill_button.UseVisualStyleBackColor = false;
+            this.kill_button.Click += new System.EventHandler(this.kill_button_Click);
+            // 
+            // MonitorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 520);
+            this.ControlBox = false;
+            this.Controls.Add(this.hide_button);
+            this.Controls.Add(this.kill_button);
+            this.Controls.Add(this.TextBox_Events);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MonitorForm";
+            this.Text = "BigBoxMonitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MonitorForm_FormClosing);
+            this.Load += new System.EventHandler(this.MonitorForm_Load);
+            this.Shown += new System.EventHandler(this.MonitorForm_Shown);
+            this.Resize += new System.EventHandler(this.MonitorForm_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox_Events;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ShowMonitorMenuItem;
+        private System.Windows.Forms.Button hide_button;
+        private System.Windows.Forms.Button kill_button;
+    }
+}
+
---
+++ OmegaBigBoxMonitor/MonitorForm.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OmegaBigBoxMonitor
+{
+    public partial class MonitorForm : Form
+    {
+        String LaunchBoxPath;
+
+        public MonitorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MonitorForm_Load(object sender, EventArgs e)
+        {
+            {
+                int MonitorsRunning = 0;
+                Process[] Processes = System.Diagnostics.Process.GetProcesses();
+                for (int i = 0; i < Processes.Length; i++)
+                {
+                    if (Processes[i].ProcessName.StartsWith("OmegaBigBoxMonitor"))
+                    {
+                        MonitorsRunning++;
+                    }
+                }
+
+                if(MonitorsRunning > 1)
+                {
+                    Application.Exit();
+                }
+            }
+
+            LaunchBoxPath = Path.GetDirectoryName(Application.ExecutablePath).ToString();
+            Log("Monitor started at " + DateTime.Now);
+            Log("---------------------------");
+            this.subscribe();
+        }
+        private void MonitorForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                if (confirm_kill() == false)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+            Log("Monitor closed at " + DateTime.Now);
+            Log("---------------------------");
+        }
+        private bool confirm_kill()
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to close the monitor? If you do, BigBox will not be automitically restarted in the event of a crash.", "Kill Omega BigBox Monitor?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void MonitorForm_Shown(object sender, EventArgs e)
+        {
+            //Start in system tray by default. 
+            this.Hide();
+        }
+
+        private void ShowMonitorMenuItem_Click(object sender, EventArgs e)
+        {
+            //Show form on right-click->show from system tray.
+            this.Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void MonitorForm_Resize(object sender, EventArgs e)
+        {
+            //On minimize, switch back to system tray
+            if (FormWindowState.Minimized == WindowState)
+                this.Hide();
+        }
+
+        private void hide_button_Click(object sender, EventArgs e)
+        {
+            //Switch to system tray when user clicks the hide button
+            this.Hide();
+        }
+
+        private void kill_button_Click(object sender, EventArgs e)
+        {
+            //Close the monitor (we will prompt for confirmation in the close event).
+            this.Close();
+        }
+
+
+        public void subscribe()
+        {
+            EventLogWatcher watcher = null;
+            try
+            {
+                EventLogQuery subscriptionQuery =
+                    new EventLogQuery("Application", PathType.LogName, "*[System/Provider/@Name=\"Application Error\"]");
+
+                watcher = new EventLogWatcher(subscriptionQuery);
+
+                // Make the watcher listen to the EventRecordWritten
+                // events.  When this event happens, the callback method
+                // (EventLogEventRead) is called.
+                watcher.EventRecordWritten +=
+                    new EventHandler<EventRecordWrittenEventArgs>(
+                        EventLogEventRead);
+
+                // Activate the subscription
+                watcher.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Error subscribing to the event log.");
+                this.Close();
+            }
+        }
+
+        
+        // Callback method that gets executed when an event is
+        // reported to the subscription.
+        public void EventLogEventRead(object obj,
+            EventRecordWrittenEventArgs arg)
+        {
+            // Make sure there was no error reading the event.
+            if (arg.EventRecord != null)
+            {
+                String eventDescription = arg.EventRecord.FormatDescription();
+
+                if (eventDescription.Contains("BigBox"))
+                {
+                    Log("BigBox crash and recovery at " + DateTime.Now);
+                    Log(eventDescription);
+                    Log("---------------------------");
+
+                    //Start external program which kills and restarts BigBox.
+                    Process ps_bigbox = null;
+                    ps_bigbox = new Process();
+                    ps_bigbox.StartInfo.UseShellExecute = false;
+                    ps_bigbox.StartInfo.RedirectStandardInput = false;
+                    ps_bigbox.StartInfo.RedirectStandardOutput = false;
+                    ps_bigbox.StartInfo.CreateNoWindow = true;
+                    ps_bigbox.StartInfo.UserName = null;
+                    ps_bigbox.StartInfo.Password = null;
+                    ps_bigbox.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    ps_bigbox.StartInfo.Arguments = "\"(Restarting BigBox)\"";
+                    ps_bigbox.StartInfo.FileName = LaunchBoxPath + "/RebootBigBox.exe";
+
+                    if (File.Exists(ps_bigbox.StartInfo.FileName))
+                    {
+                        bool result = ps_bigbox.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Missing " + ps_bigbox.StartInfo.FileName);
+                    }
+
+                }
+            }
+        }
+        public void Log(String info)
+        {
+            String LogFileName = LaunchBoxPath + "/Logs/BigBoxMonitorLog.txt";
+            StreamWriter sw = null;
+
+            if (!Directory.Exists(Path.GetDirectoryName(LogFileName)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(LogFileName));
+            }
+
+            if (!File.Exists(LogFileName))
+            {
+                // Create a file to write to.
+                sw = File.CreateText(LogFileName);
+            }
+            else
+            {
+                sw = File.AppendText(LogFileName);
+            }
+
+            if (sw != null)
+            {
+                sw.WriteLine(info);
+                sw.Flush();
+                sw.Close();
+            }
+
+            TextBox_Events.AppendText(info);
+            TextBox_Events.AppendText("\r\n");
+        }
+
+    }
+}
